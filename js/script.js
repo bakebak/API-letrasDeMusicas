66a2f774f8c791c06dd4a142f298646a42ffd254
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-27 12:03:46 -0300

    adição da tela com informações da banda

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
 var host = {urlProduct:"http://localhost:3000/product"};
 
-$(document).ready(function(){  
-    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-
-            // Prevent default anchor click behavior
-             event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-        
-            // volta ao topo
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 900, function(){
-       
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+$(document).ready(function(){
+    telaInicial();
+    $("#btnPesquisar").click(function(){
+        $("#exibirTabela").show();
+        $("#exibirArtista").hide()
     });
-    $("#btnListaFrutas").click(function(){
-        chamaLista();
+    $("#btnDetalhes").click(function(){
+        $("#exibirTabela").hide();
+        $("#exibirArtista").show();
     });
+    $("#btnVoltar").click(function(){
+        telaInicial();
+    });
+    
+});
 
-})
-
-function chamaLista(){
-    $.getJSON(host.urlProduct, function (list){
-        var lista = '';
-        var i;        
-        for(i=0; i < list.length; i++){
-            lista += "<span class='"+list[i].status+"'>"+ list[i].id + " - " + list[i].nome + " - R$ " + list[i].valor + " - " + list[i].estoque +"</span><br>";
-            valorCadaProduto = list[i].valor * list[i].estoque;
-        $("#listaFrutas").html(lista);
+$(document).keypress(function(e) {
+    if (e.which == 13) {
+        $("#exibirTabela").show();
     }
-    })
+});
+
+function teste(){
+    
+}
+    
+/*function pesquisar(){
+    var entrada = $("#numero").val();
+        if (entrada !== ""){
+            chamaIndividual(entrada);
+            ocultarAdicionar ();
+        }
+        
+        else if (entrada == "") {
+            $("#dados").html(mensagens.disponibilidade);    
+            ocultarNaoDisponivel();
+        }
+        $("#dados").show();
+}*/
+
+function telaInicial() {
+    $("#exibirTabela").hide();  
+    $("#exibirArtista").hide();
 }
