824b4228d51dd26cb36a74cdf7de8e73e27a15dd
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-27 15:30:56 -0300

    requisição só com o artista

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-var host = {urlProduct:"http://localhost:3000/product"};
+var host = {urlArtista:"https://www.vagalume.com.br/",
+            urlArtistaNaoEncontrado:"https://api.vagalume.com.br/search.art?q="};
+
 
 $(document).ready(function(){
     telaInicial();
     $("#btnPesquisar").click(function(){
-        $("#exibirTabela").show();
-        $("#exibirArtista").hide()
+        chamaArtista();
     });
     $("#btnDetalhes").click(function(){
         $("#exibirTabela").hide();
@@ -18,27 +19,35 @@ $(document).ready(function(){
 
 $(document).keypress(function(e) {
     if (e.which == 13) {
-        $("#exibirTabela").show();
+        chamaArtista();
     }
 });
-
-function teste(){
     
+function chamaArtista (){
+   var entradaArtista = $("#digitaArtista").val();
+   var entradaComHifen = entradaArtista.replace(/ /gi, "-");
+    $.getJSON(host.urlArtista+entradaComHifen+"/index.js", function (list){
+        var musicas = '';
+        var artista = '';
+        var i;
+        artista += list.artist.desc;
+        for(i=0; i < list.artist.toplyrics.item.length; i++){
+            musicas += list.artist.toplyrics.item[i].desc;
+        }
+        $("#telaResultado").html(artista+musicas);
+    })
+    /*.fail(function() {
+       $.getJSON(host.urlArtistaNaoEncontrado+entradaComHifen+"&limit=5", function (list){
+            var banda = '';
+            var i;
+            for(i=0; i < list.docs.length; i++){
+                banda += list.docs[i].band;
+            }
+            $("#telaResultado").html(banda);
+            console.log(banda);
+        })
+    })*/
 }
-    
-/*function pesquisar(){
-    var entrada = $("#numero").val();
-        if (entrada !== ""){
-            chamaIndividual(entrada);
-            ocultarAdicionar ();
-        }
-        
-        else if (entrada == "") {
-            $("#dados").html(mensagens.disponibilidade);    
-            ocultarNaoDisponivel();
-        }
-        $("#dados").show();
-}*/
 
 function telaInicial() {
     $("#exibirTabela").hide();  
